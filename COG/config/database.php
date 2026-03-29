14ac8b959d1c61c18106efa24019fba6df688b16
LinkinBry <bryanjaylicuanandomingo@gmail.com>
2026-03-29 05:32:34 +0000

    sakses sa db

--- COG/config/database.php
+++ COG/config/database.php
@@ -1,17 +1,20 @@
 <?php
 // config/database.php
 class Database {
-    private $host = "localhost";
+    private $host = "127.0.0.1";   // ✅ force TCP
     private $db_name = "cog_management_system";
-    private $username = "root";
-    private $password = "";
+    private $username = "cog_user";
+    private $password = "yourpassword";
     public $conn;
 
     public function getConnection() {
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
-                                  $this->username, $this->password);
+            $this->conn = new PDO(
+                "mysql:host={$this->host};port=3306;dbname={$this->db_name}",
+                $this->username,
+                $this->password
+            );
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->exec("set names utf8");
         } catch(PDOException $exception) {
